3e355ca6c4cd988b416c1442f1a43ff6da050706
Albin Samefors <81687105+albinSamefors@users.noreply.github.com>
2023-03-14 11:53:41 +0100

    small setups

---
+++ micropython/STM32_scripts/interrupt_tests.py
@@ -0,0 +1,11 @@
+import machine
+import time
+def lightsleep_test(interval_in_ms, duration_in_s):
+    runs = 0
+
+    pass
+
+def deepsleep_test(interval_in_ms, duration_in_s):
+    runs = 0
+    pass
+
---
+++ micropython/main.py
@@ -0,0 +1,2 @@
+def run():
+    from STM32_scripts import interrupt_tests, set_interval_tests
