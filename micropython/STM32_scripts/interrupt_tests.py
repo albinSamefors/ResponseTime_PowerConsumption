caf384133f7cd5f88d2eeb9b229443765f87d944
Albin Samefors <81687105+albinSamefors@users.noreply.github.com>
2023-03-15 09:13:45 +0100

    Timing done from within

--- micropython/STM32_scripts/interrupt_tests.py
+++ micropython/STM32_scripts/interrupt_tests.py
@@ -1,18 +1,21 @@
 from machine import Pin, lightsleep, deepsleep
 
-PIN_NUMBER = 0
+PIN_NUMBER = 'A1'
 CALLBACK_PIN = Pin(PIN_NUMBER, Pin.OUT, Pin.PULL_DOWN)
 
 def callback():
     CALLBACK_PIN.on()
     CALLBACK_PIN.off()
 
-def lightsleep_test(interval_in_ms):
-    lightsleep(interval_in_ms)
+def lightsleep_blinky_test():
+    pass
+
+def lightsleep_i2C_test():
+    lightsleep()
     callback()
 
-def deepsleep_test(interval_in_ms):
-    deepsleep(interval_in_ms)
+def deepsleep_i2C_test():
+    deepsleep()
     callback()
 
 
