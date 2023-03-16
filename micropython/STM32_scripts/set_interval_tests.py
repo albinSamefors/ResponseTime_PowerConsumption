8a3029abc1e34c4e46f0ee6ced976bd2579c0791
Albin Samefors <81687105+albinSamefors@users.noreply.github.com>
2023-03-16 12:28:11 +0100

    Added new lightsleep functionality
    WARNING: THIS IS KINDA WONKY AND MIGHT NOT WORK AS EXPECTED

--- micropython/STM32_scripts/set_interval_tests.py
+++ micropython/STM32_scripts/set_interval_tests.py
@@ -1,23 +1,34 @@
 import machine
-import time
+import utime
 
 
 def lightsleep_test(interval_in_ms, duration_in_ms):
+    print("ENTERED LIGHTSLEEP TEST")
     results = []
-    test_start = time.ticks_us()
-    while time.ticks_diff(time.ticks_us(), test_start) < duration_in_ms * 1000:
-        timer_start = time.ticks_us()
+    test_start = utime.ticks_cpu()
+    time_slept = 0
+    print(test_start)
+    while utime.ticks_diff(utime.ticks_cpu(), test_start) + time_slept < utime.ticks_diff(test_start + (duration_in_ms * 1000 * 64), test_start):
+        print("Tick NOW:")
+        print(utime.ticks_diff(utime.ticks_cpu(), test_start))
+        print("TICK GOAL:")
+        print(utime.ticks_diff(test_start + (duration_in_ms * 1000 * 64), test_start))
+        timer_start = utime.ticks_cpu()
         machine.lightsleep(interval_in_ms)
-        timer_end = time.ticks_us()
-        results.append(time.ticks_diff(timer_start,timer_end) - (interval_in_ms * 1000))
+        timer_end = utime.ticks_cpu()
+        time_slept += interval_in_ms * 64
+        results.append([(utime.ticks_diff(timer_end,timer_start)/64),
+                        (utime.ticks_diff(timer_start,test_start)/64),
+                        (utime.ticks_diff(timer_end, test_start)/64)])
+        print("RESULT APPENDED")
     return results
 
 def deepsleep_test(interval_in_ms, duration_in_ms):
     results = []
-    test_start = time.ticks_us()
-    while time.ticks_diff(time.ticks_us(), test_start) < duration_in_ms * 1000:
-        timer_start = time.ticks_us()
+    test_start = utime.ticks_cpu()
+    while utime.ticks_diff(utime.ticks_cpu(), test_start) < duration_in_ms * 1000:
+        timer_start = utime.ticks_cpu()
         machine.deepsleep(interval_in_ms)
-        timer_end = time.ticks_us()
-        results.append(time.ticks_diff(timer_start, timer_end) - (interval_in_ms * 1000))
+        timer_end = utime.ticks_cpu()
+        results.append(utime.ticks_diff(timer_end, timer_start) - (interval_in_ms * 1000))
     return results
--- micropython/main.py
+++ micropython/main.py
@@ -1,5 +1,11 @@
-def run():
-    from STM32_scripts import interrupt_tests, set_interval_tests
+from STM32_scripts import interrupt_tests, set_interval_tests
+import machine
+print("EXPERIMENT STARTING POINT")
 
-    times = set_interval_tests.lightsleep_test(100, 500 * 1000000)
-    print(times)
+times = set_interval_tests.lightsleep_test(10, 1 * 1000)
+
+#print("MACHINE FREQ {}".format(machine.freq()))
+
+print("ALL TIMES: \n")
+for time in times:
+    print("TIME: {}, Time started {}, Time Stopped {} \n".format(time[0], time[1], time[2]))
